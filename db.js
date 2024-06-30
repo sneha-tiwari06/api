21045cefc35b61832d2ad2bbe3a2b547e4b53135
Sneha Tiwari <snehatiwari0620@gmail.com>
2024-06-30 16:32:39 +0530

    m

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ import mysql from "mysql";
 
 export const db = mysql.createConnection({
   host: "23.83.74.97",
-  user: "root",
-  password: "Sneha@2012",
-  database: "smartex_db"
+  user: "ecis_smaruser",
+  password: "W]u@,^=7DX^i",
+  database: "ecis_smartex"
 });
 console.log("db connect")
