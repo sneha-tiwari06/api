4741949d57f6c460ba00393c8324fb652aef1908
Sneha Tiwari <snehatiwari0620@gmail.com>
2024-06-30 13:11:50 +0530

    first

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
-import mysql from "mysql"
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password: "Sneha@2012",
-  database:"smartex"
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
